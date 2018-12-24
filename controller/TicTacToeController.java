7baea965c100ba5faa97a974a6e23ed3c60dd657
Dziusau <43704303+Dziusau@users.noreply.github.com>
2018-12-24 12:39:32 +0300

    Add files via upload

--- controller/TicTacToeController.java
+++ controller/TicTacToeController.java
@@ -3,68 +3,184 @@ package tictactoe.controller;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import sun.awt.NullComponentPeer;
 
-public class TicTacToeController {
+import javax.lang.model.type.NullType;
+
+public class TicTacToeController {;
     @FXML
     private Button buttonR0C0;
     @FXML
-    private Button sign2;
+    private Button buttonR0C1;
     @FXML
-    private Button sign3;
+    private Button buttonR0C2;
     @FXML
-    private Button sign4;
+    private Button buttonR1C0;
     @FXML
-    private Button sign5;
+    private Button buttonR1C1;
     @FXML
-    private Button sign6;
+    private Button buttonR1C2;
     @FXML
-    private Button sign7;
+    private Button buttonR2C0;
     @FXML
-    private Button sign8;
+    private Button buttonR2C1;
     @FXML
-    private Button sign9;
+    private Button buttonR2C2;
+
+    int i;
+
+    public void checkWin() {
+        if (buttonR0C0.getText() == buttonR0C1.getText() || buttonR0C0.getText() == buttonR0C2.getText()) {
+            System.out.println("Вы победили");
+        } else if (buttonR0C0.getText() == buttonR1C1.getText() || buttonR0C0.getText() == buttonR2C2.getText()) {
+            System.out.println("Вы победили");
+        } else if (buttonR0C0.getText() == buttonR1C0.getText() || buttonR0C0.getText() == buttonR2C0.getText()) {
+            System.out.println("Вы победили");
+        } else if (buttonR0C1.getText() == buttonR1C1.getText() || buttonR0C1.getText() == buttonR2C1.getText()) {
+            System.out.println("Вы победили");
+        } else if (buttonR0C2.getText() == buttonR1C1.getText() || buttonR0C2.getText() == buttonR2C0.getText()) {
+            System.out.println("Вы победили");
+        } else if (buttonR0C2.getText() == buttonR1C2.getText() || buttonR0C2.getText() == buttonR2C2.getText()) {
+            System.out.println("Вы победили");
+        } else if (buttonR1C0.getText() == buttonR1C1.getText() || buttonR1C0.getText() == buttonR1C2.getText()) {
+            System.out.println("Вы победили");
+        } else if (buttonR2C0.getText() == buttonR2C1.getText() || buttonR2C0.getText() == buttonR2C2.getText()) {
+            System.out.println("Вы победили");
+        }
+    }
 
     public void button1Clicked(javafx.event.ActionEvent actionEvent) {
         System.out.println("Button1 clicked");
-        buttonR0C0.setText("X");
+        i++;
+        if (buttonR0C0.getText() == null){
+            if ((i % 2) == 0) {
+                buttonR0C0.setText("O");
+            } else {
+                buttonR0C0.setText("X");
+            }
+        } else {
+            System.out.println("Кнопка заблокирована");
+        }
+        checkWin();
     }
 
     public void button2Clicked(javafx.event.ActionEvent actionEvent) {
         System.out.println("Button2 clicked");
-        ticTac2.set("O");
+        i++;
+        if (buttonR0C1.getText() == null){
+            if ((i % 2) == 0) {
+                buttonR0C1.setText("O");
+            } else {
+                buttonR0C1.setText("X");
+            }
+        } else {
+                System.out.println("Кнопка заблокирована");
+        }
+        checkWin();
     }
 
     public void button3Clicked(javafx.event.ActionEvent actionEvent) {
         System.out.println("Button3 clicked");
-        ticTac3.set("X");
+        i++;
+        if (buttonR0C2.getText() == null){
+            if ((i % 2) == 0) {
+                buttonR0C2.setText("O");
+            } else {
+                buttonR0C2.setText("X");
+            }
+        } else {
+            System.out.println("Кнопка заблокирована");
+        }
+        checkWin();
     }
 
     public void button4Clicked(javafx.event.ActionEvent actionEvent) {
         System.out.println("Button4 clicked");
-        ticTac4.set("X");
+        i++;
+            if (buttonR1C0.getText() == null){
+                if ((i % 2) == 0) {
+                buttonR1C0.setText("O");
+            } else {
+                buttonR1C0.setText("X");
+            }
+        } else {
+            System.out.println("Кнопка заблокирована");
+        }
+        checkWin();
     }
 
     public void button5Clicked(javafx.event.ActionEvent actionEvent) {
         System.out.println("Button5 clicked");
-        ticTac5.set("O");
+        i++;
+        if (buttonR1C1.getText() == null){
+            if ((i % 2) == 0) {
+                buttonR1C1.setText("O");
+            } else {
+                buttonR1C1.setText("X");
+            }
+        } else {
+            System.out.println("Кнопка заблокирована");
+        }
+        checkWin();
     }
 
     public void button6Clicked(javafx.event.ActionEvent actionEvent) {
         System.out.println("Button6 clicked");
-        ticTac6.set("X");
+        i++;
+        if (buttonR1C2.getText() == null){
+            if ((i % 2) == 0) {
+                buttonR1C2.setText("O");
+            } else {
+                buttonR1C2.setText("X");
+            }
+        } else {
+            System.out.println("Кнопка заблокирована");
+        }
+        checkWin();
     }
 
     public void button7Clicked(javafx.event.ActionEvent actionEvent) {
         System.out.println("Button7 clicked");
-        ticTac7.set("X");
+        i++;
+        if (buttonR2C2.getText() == null){
+            if ((i % 2) == 0) {
+                buttonR2C0.setText("O");
+            } else {
+                buttonR2C0.setText("X");
+            }
+        } else {
+            System.out.println("Кнопка заблокирована");
+        }
+        checkWin();
     }
 
     public void button8Clicked(javafx.event.ActionEvent actionEvent) {
         System.out.println("Button8 clicked");
-        ticTac8.set("O");
+        i++;
+        if (buttonR2C1.getText() == null){
+            if ((i % 2) == 0) {
+                buttonR2C1.setText("O");
+            } else {
+                buttonR2C1.setText("X");
+            }
+        } else {
+            System.out.println("Кнопка заблокирована");
+        }
+        checkWin();
     }
+
     public void button9Clicked(javafx.event.ActionEvent actionEvent) {
         System.out.println("Button9 clicked");
-        ticTac9.set("O");
+        i++;
+        if (buttonR0C0.getText() == null){
+            if ((i % 2) == 0) {
+                buttonR2C2.setText("O");
+            } else {
+                buttonR2C2.setText("X");
+            }
+        } else {
+            System.out.println("Кнопка заблокирована");
+        }
+        checkWin();
     }
 }
